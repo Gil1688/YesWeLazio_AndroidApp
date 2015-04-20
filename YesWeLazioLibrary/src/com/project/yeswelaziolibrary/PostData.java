b97540ecb5dff8f328e662a4b11aef0c339017e3
Gabriele <gidanillaziale@gmail.com>
2015-04-20 16:21:54 +0200

    Merge All Existing Files

---
+++ YesWeLazioLibrary/src/com/project/yeswelaziolibrary/PostData.java
@@ -0,0 +1,34 @@
+package com.project.yeswelaziolibrary;
+
+public class PostData {
+	public String postThumbUrl;
+    public String postTitle;
+    public String postDate;
+    public String postLink;
+    public String postContent;
+    public String postGuid;
+    
+    public String getThumbUrl () {
+    	return postThumbUrl;
+    }
+    
+    public String getTitle () {
+    	return postTitle;
+    }
+    
+    public String getDate () {
+    	return postDate;
+    }
+    
+    public String getLink () {
+    	return postLink;
+    }
+    
+    public String getContent () {
+    	return postContent;
+    }
+    
+    public String getGuid () {
+    	return postGuid;
+    }
+}
